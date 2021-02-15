14cb4fe3b57f486eee6d9c725303b840b18737a2
Timothy <timothylam96@hotmail.com>
2021-02-15 23:02:33 +0800

    own multi machine JSP working

--- main.py
+++ main.py
@@ -62,17 +62,7 @@ def flexible_job_shop_problem():
     horizon = sum(task for job in jobs_data for task in job)
 
     print("Horizon:", horizon)
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    # Named tuple to store information about created variables.
-    task_type = collections.namedtuple('task_type', 'start end interval assign')
+
     # Named tuple to manipulate solution information.
     assigned_task_type = collections.namedtuple('assigned_task_type',
                                                 'start job index duration')
@@ -80,95 +70,72 @@ def flexible_job_shop_problem():
     # Creates job intervals and add to the corresponding machine lists.
     all_tasks = {}
     starts = {}
+    ends = {}
     machine_to_intervals = collections.defaultdict(list)
-    job_ends = []
 
     for job_id, job in enumerate(jobs_data):
-        previous_end = None
         for task_id, task in enumerate(job):
+
             l_assign = []
+            suffix = '_%i_%i' % (job_id, task_id)
 
-            min_duration = min(job)
-            max_duration = max(job)
+            # add common start
+            start = model.NewIntVar(0, horizon, 'start_ori_' + suffix)
+            end = model.NewIntVar(0, horizon, 'end_ori_' + suffix)
 
-            # Create main interval for the task.
-            suffix_name = '_j%i_t%i' % (job_id, task_id)
-            start1 = model.NewIntVar(0, horizon, 'start1' + suffix_name)
-            duration = model.NewIntVar(min_duration, max_duration,
-                                       'duration' + suffix_name)
-            end1 = model.NewIntVar(0, horizon, 'end1' + suffix_name)
-            interval1 = model.NewIntervalVar(start1, duration, end1,
-                                            'interval1' + suffix_name)
-
-            starts[(job_id, task_id)] = start1
-
-            if previous_end:
-                model.Add(start1 >= previous_end)
-            previous_end = end1
+            starts[(job_id, task_id)] = start
+            ends[(job_id, task_id)] = end
 
             for machine_id, machine in enumerate(all_machines):
                 # add machine no overlap
-                suffix = '_%i_%i' % (job_id, task_id)
+                assign = model.NewBoolVar('assigned' + suffix)
                 start_var = model.NewIntVar(0, horizon, 'start' + suffix)
                 end_var = model.NewIntVar(0, horizon, 'end' + suffix)
-                assign = model.NewBoolVar('assigned' + suffix)
-                interval_var = model.NewOptionalIntervalVar(start_var, duration, end_var, assign,
-                                                            'interval' + suffix)
-                machine_to_intervals[machine].append(interval_var)
+                interval_var = model.NewIntervalVar(start_var, task, end_var,
+                                                    'interval' + suffix)
 
-                model.Add(start1 == start_var).OnlyEnforceIf(l_assign)
-                model.Add(end1 == end_var).OnlyEnforceIf(l_assign)
+                model.Add(start == start_var).OnlyEnforceIf(assign)
+                model.Add(end == end_var).OnlyEnforceIf(assign)
+
+                machine_to_intervals[machine].append(interval_var)
 
                 l_assign.append(assign)
                 all_tasks[job_id, task_id, machine_id] = assign
 
             model.Add(sum(l_assign) == 1)
-        job_ends.append(previous_end)
 
     for machine in all_machines:
         model.AddNoOverlap(machine_to_intervals[machine])
 
     # Precedences inside a job.
-    # for job_id, job in enumerate(jobs_data):
-    #     for task_id in range(len(job) - 1):
-    #         for machine_id in range(len(all_machines)):
-    #             for machine_id1 in range(len(all_machines) - machine_id):
-    #                 model.Add(all_tasks[job_id, task_id + 1, machine_id1].start
-    #                           >= all_tasks[job_id, task_id, machine_id].end)
+    for job_id, job in enumerate(jobs_data):
+        for task_id in range(len(job) - 1):
+            model.Add(starts[job_id, task_id + 1]
+                      >= ends[job_id, task_id])
 
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
     # Makespan objective.
-    obj_var = model.NewIntVar(0, horizon, 'makespan')
-    model.AddMaxEquality(obj_var, job_ends)
-    model.Minimize(obj_var)
+    makespan = model.NewIntVar(0, horizon, 'makespan')
+    model.AddMaxEquality(makespan, [ends[(job_id, len(job) - 1)]
+                                    for job_id, job in enumerate(jobs_data)])
+    model.Minimize(makespan)
 
     # Solve model.
     solver = cp_model.CpSolver()
     solution_printer = SolutionPrinter()
-    status = solver.SolveWithSolutionCallback(model, solution_printer)
+    solver.SolveWithSolutionCallback(model, solution_printer)
 
     # Create one list of assigned tasks per machine.
     assigned_jobs = collections.defaultdict(list)
     for job_id, job in enumerate(jobs_data):
         for task_id, task in enumerate(job):
             for machine_id, machine in enumerate(all_machines):
-                assigned_jobs[machine].append(
-                    assigned_task_type(
-                        start=solver.Value(starts[(job_id, task_id)]),
-                        job=job_id,
-                        index=task_id,
-                        duration=task))
+                if solver.Value(all_tasks[job_id, task_id, machine_id]):
+                    assigned_jobs[machine].append(
+                        assigned_task_type(
+                            start=solver.Value(starts[(job_id, task_id)]),
+                            job=job_id,
+                            index=task_id,
+                            duration=task))
 
     # Create per machine output lines.
     output = ''
@@ -222,24 +189,6 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
 def flexible_jobshop():
     """Solve a small flexible jobshop problem."""
     # Data part.
-    jobs1 = [ # task = (processing_time, machine_id)
-        [ # Job 0
-            [(3, 0), (1, 1), (5, 2)], # task 0 with 3 alternatives
-            [(2, 0), (4, 1), (6, 2)], # task 1 with 3 alternatives
-            [(2, 0), (3, 1), (1, 2)], # task 2 with 3 alternatives
-        ],
-        [ # Job 1
-            [(2, 0), (3, 1), (4, 2)],
-            [(1, 0), (5, 1), (4, 2)],
-            [(2, 0), (1, 1), (4, 2)],
-        ],
-        [ # Job 2
-            [(2, 0), (1, 1), (4, 2)],
-            [(2, 0), (3, 1), (4, 2)],
-            [(3, 0), (1, 1), (5, 2)],
-        ],
-    ]
-
     jobs = [
         [3, 2, 2],
         [2, 1, 4],
@@ -257,8 +206,7 @@ def flexible_jobshop():
 
     horizon = 0
     for job in jobs:
-        max_task_duration = sum(job)
-        horizon += max_task_duration
+        horizon += sum(job)
 
     print('Horizon = %i' % horizon)
 
@@ -276,14 +224,9 @@ def flexible_jobshop():
         for task_id in range(num_tasks):
             task = job[task_id]
 
-            min_duration = task
-            max_duration = task
-
             # Create main interval for the task.
             suffix_name = '_j%i_t%i' % (job_id, task_id)
             start = model.NewIntVar(0, horizon, 'start' + suffix_name)
-            duration = model.NewIntVar(min_duration, max_duration,
-                                       'duration' + suffix_name)
             end = model.NewIntVar(0, horizon, 'end' + suffix_name)
 
             # Store the start for the solution.
@@ -300,16 +243,14 @@ def flexible_jobshop():
                 alt_suffix = '_j%i_t%i_a%i' % (job_id, task_id, machine_id)
                 l_presence = model.NewBoolVar('presence' + alt_suffix)
                 l_start = model.NewIntVar(0, horizon, 'start' + alt_suffix)
-                l_duration = task
                 l_end = model.NewIntVar(0, horizon, 'end' + alt_suffix)
                 l_interval = model.NewOptionalIntervalVar(
-                    l_start, l_duration, l_end, l_presence,
+                    l_start, task, l_end, l_presence,
                     'interval' + alt_suffix)
                 l_presences.append(l_presence)
 
                 # Link the master variables with the local ones.
                 model.Add(start == l_start).OnlyEnforceIf(l_presence)
-                model.Add(duration == l_duration).OnlyEnforceIf(l_presence)
                 model.Add(end == l_end).OnlyEnforceIf(l_presence)
 
                 # Add the local interval to the right machine.
@@ -352,7 +293,7 @@ def flexible_jobshop():
                     selected = machine_id
             print(
                 '  task_%i_%i starts at %i (machine %i, duration %i, end %i)' %
-                (job_id, task_id, start_value, selected, duration, start_value+duration))
+                (job_id, task_id, start_value, selected, duration, start_value + duration))
 
     print('Solve status: %s' % solver.StatusName(status))
     print('Optimal objective value: %i' % solver.ObjectiveValue())
@@ -365,5 +306,5 @@ def flexible_jobshop():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
-    flexible_jobshop()
+    flexible_job_shop_problem()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
